c4a32ed2006d5c901f3f6440f2b862eb18ca39e8
hawrot <mhawro200@caledonian.ac.uk>
2020-02-03 16:38:17 +0000

    Fix name on the post

--- controllers/feed.js
+++ controllers/feed.js
@@ -14,6 +14,7 @@ exports.getPosts = async (req, res, next) => {
   try {
     const totalItems = await Post.find().countDocuments();
     const posts = await Post.find()
+        .populate('creator')
       .skip((currentPage - 1) * perPage)
       .limit(perPage);
 
@@ -56,7 +57,7 @@ exports.createPost = async (req, res, next) => {
     const user = await User.findById(req.userId);
     user.posts.push(post);
     await user.save();
-    io.getIO().emit('posts', { action: 'create', post: post });
+    io.getIO().emit('posts', { action: 'create', post: {...post, creator : {_id : req.userId} }});
     res.status(201).json({
       message: 'Post created successfully!',
       post: post,
